fe55d4bb0259f6ae0ab5c7f57d208254b485cc44
Golenia <lucas.golenia@atos.net>
2019-10-01 08:44:26 +0200

    moves creds file

--- src/app/shared/api.service.ts
+++ src/app/shared/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { CREDENTIALS } from './credentials';
+import { CREDENTIALS } from '../../environments/credentials';
 
 @Injectable({
   providedIn: 'root'
